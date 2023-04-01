b19c47078d5cccec109f047a4901b9b505f4eb66
root <showell30@yahoo.com>
2023-04-01 19:10:38 +0000

    tests

---
+++ test_calc.js
@@ -0,0 +1,39 @@
+assert = require("assert");
+calc = require("./calc");
+
+function run_test(s, f) { f();}
+
+run_test("double", () => {
+    assert.equal(calc.double(1), 2);
+    assert.equal(calc.double(2), 4);
+    assert.equal(calc.double(3), 6);
+});
+
+run_test("triple", () => {
+    assert.equal(calc.triple(1), 3);
+    assert.equal(calc.triple(2), 6);
+    assert.equal(calc.triple(3), 9);
+});
+
+run_test("calculate", () => {
+    assert.deepEqual(
+        calc.calculate([1, 2, 3], calc.double),
+        [[1, 2], [2, 4], [3, 6]],
+    );
+    assert.deepEqual(
+        calc.calculate([1, 2, 3], calc.triple),
+        [[1, 3], [2, 6], [3, 9]],
+    );
+});
+
+run_test("abstract_plotter", () => {
+    called = false;
+    function mock_plotter(tups) {
+        assert.deepEqual(tups, [[1, 6], [2, 12], [3, 18], [4, 24]]);
+        called = true;
+    }
+
+    f = (x) => calc.double(calc.triple(x));
+    calc.plot_function_with_plotter([1, 2, 3, 4], f, mock_plotter)
+    assert.equal(called, true)
+});
